f439db2861bf30818042bfe4d0bffa80d1076780
Raphael B <rapha18023@gmail.com>
2023-06-22 12:47:52 -0300

    Service layer, component registration

---
+++ src/main/java/com/ProjectWebService/courseSpring/services/UserService.java
@@ -0,0 +1,25 @@
+package com.ProjectWebService.courseSpring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ProjectWebService.courseSpring.entities.User;
+import com.ProjectWebService.courseSpring.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
